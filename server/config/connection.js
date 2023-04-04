bcf63b2ffc797ce4592a17241d963ba2e118c7ef
Johnny <johnathan.mnn@gmail.com>
2023-04-03 22:06:46 -0500

    Added database name to connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1/your-database-name',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1/goalie',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
