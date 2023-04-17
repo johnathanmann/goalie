3cfe82614db048cc6cc6acd520cdc8368af05630
Johnny <johnathan.mnn@gmail.com>
2023-04-16 21:22:16 -0500

    Lightmode/darkmode icon changings src on click

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,15 @@
-import React from "react"
+import React, {useState} from "react"
 import "../styles/theme.css"
+import darkmodeToggler from "../assets/imgs/darkmode-toggler.png";
+import lightmodeToggler from "../assets/imgs/lightmode-toggler.png";
 
 const Nav = () => {
+  const [lightMode, setLightMode ] = React.useState(true)
   let clickedClass = "clicked"
   const body = document.body
   const lightTheme = "light"
   const darkTheme = "dark"
-  let theme
+  let theme;
 
   if (localStorage) {
     theme = localStorage.getItem("theme")
@@ -18,7 +21,8 @@ const Nav = () => {
     body.classList.add(lightTheme)
   }
 
-  const switchTheme = e => {
+  async function switchTheme (e) {
+    setLightMode(!lightMode)
     if (theme === darkTheme) {
       body.classList.replace(darkTheme, lightTheme)
       e.target.classList.remove(clickedClass)
@@ -33,12 +37,14 @@ const Nav = () => {
   }
 
   return (
-    <label class="switch">
+    <div id="nav">
+    <img src={lightMode === true ? darkmodeToggler : lightmodeToggler}/><label className="switch">
     <input type="checkbox" className={theme === "dark" ? clickedClass : ""}
       id="darkMode"
       onClick={e => switchTheme(e)}/>
-    <span class="slider round"></span>
+    <span className="slider round"></span>
     </label>
+    </div>
   )
 }
 
